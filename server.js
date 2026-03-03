69f546c5005d4f243f5555152302fe3766800032
马俊 <majun@majundeMacBook-Air.local>
2026-03-03 13:42:35 +0800

    Fix Railway bind address

--- server.js
+++ server.js
@@ -218,6 +218,6 @@ app.post("/fetch-url", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
